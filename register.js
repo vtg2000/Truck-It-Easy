ca200a4e236604fa4c123e9f87ba285f7b8e7f3e
Vignesh Viswanathan <vigneshvisw@gmail.com>
2019-07-23 12:27:06 +0530

    Add files via upload

---
+++ register.js
@@ -0,0 +1,19 @@
+
+
+function test() {
+  console.log('i am here')
+  var obj = {};
+
+  obj.title =document.getElementById('name').value
+//   console.log('title: '+obj.title);
+
+//   obj.userID = document.getElementById('#userID').prop('value');
+//   console.log('userID: '+obj.website);
+
+  obj.passCode =document.getElementById('password').value
+//   console.log('password: '+obj.tags);
+
+  console.log(obj)
+  
+  
+} 
