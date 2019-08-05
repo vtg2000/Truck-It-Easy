b4658d21fb8b050f889d7388e3fcfd2e4ba14dc9
vtg2000 <vigneshvisw@gmail.com>
2019-08-05 22:06:36 +0530

    few css changes

--- html/login.php
+++ html/login.php
@@ -11,12 +11,12 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/register.css">
-    <title>Login</title>
+    <title style='color: white'>Login</title>
 </head>
-<body style='background-image: linear-gradient(lightblue, rgb(3, 121, 160)); height: 100vh;'>
+<body style='background-image: linear-gradient(to right, #780206,#061161); height: 100vh;'>
         
         <div  >
-        <h2 style='margin-top: 100px;  ' class='text-center'>Login</h2>
+        <h2 style='margin-top: 100px; color: white ' class='text-center'>Login</h2>
         <form action="home.php" id="Myform" method='post' >
             <div style = 'display:flex; color:white; flex-direction: column; margin-left: 40vw; background-image:linear-gradient( rgb(122, 113, 252), rgb(241, 11, 192)); margin-right:40vw; padding: 2vw; border-radius: 20px'>
             
