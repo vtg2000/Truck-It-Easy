ae9cda4cb209361ef66fd375edffb3e55f68e8ee
vtg2000 <vigneshvisw@gmail.com>
2019-08-10 17:41:19 +0530

    few fixes

--- html/login.php
+++ html/login.php
@@ -14,12 +14,14 @@ session_destroy();
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
         integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="../css/register.css">
+    <link rel="stylesheet" href="../css/welcome.css">
+
     <title style='color: white'>Login</title>
 </head>
 
 <body style='background-image: linear-gradient(to right, #780206,#061161);'>
     <nav class="navbar navbar-dark fixed-top">
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="index.html">
             <div class='text-center'>Truck It Easy</div>
         </a>
         <div style='display:flex; flex-direction: row;'>
@@ -27,11 +29,11 @@ session_destroy();
             <a class="nav-item nav-link" href="index.html" style="text-decoration:none; color:white;">Home</a>
         </div>
     </nav>
-    <div>
+    <div class='animate-reveal animate-first'>
         <h2 style='margin-top: 100px; color: white ' class='text-center'>Login</h2>
         <form action="validate.php" id="Myform" method='post'>
             <div
-                style='display:flex; color:white; flex-direction: column; margin-left: 40vw; background-image:linear-gradient( #9D50BB, #6E48AA); margin-right:40vw; padding: 2vw; border-radius: 20px'>
+                style='display:flex; color:white; flex-direction: column; margin-left: 40vw; background-image:linear-gradient( #9D50BB, #6E48AA); margin-right:40vw; padding: 1.5vw; border-radius: 20px'>
 
                 <label class='formLabel' for="">Email</label>
                 <input required class='formInput' name="email">
@@ -44,7 +46,7 @@ session_destroy();
                         style='width:100px; margin:auto; display:block'>Login</button>
                 </div>
                 <br>
-                <p style='font-size:12px'>Don't have an account? <a href='register.html'>Register!</a></p>
+                <p style='font-size:12px'>Don't have an account? <a href='register.html'>Register now!</a></p>
 
 
             </div>
