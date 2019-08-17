7966c241a81981d8cfa375b9c8a183b4488822c1
vtg2000 <vigneshvisw@gmail.com>
2019-08-18 01:49:01 +0530

    some css changes

--- html/app/sidebar.php
+++ html/app/sidebar.php
@@ -3,8 +3,8 @@
  <?php if($_SESSION['user1']):
  ?>
  <nav class="navbar navbar-dark fixed-top">
-   <a class="navbar-brand" href="#" >Truck It Easy</a>
-   <a class="nav-item nav-link" style="color:white"><?php echo $_SESSION["user1"][0];  ?></a>
+   <a class="navbar-brand" href="../app/home.php" >Truck It Easy!</a>
+   <a class="nav-item nav-link ml-auto" style="color:white;"><?php echo $_SESSION["user1"][0];  ?></a>
    <a class="nav-item nav-link" href="../auth/login.php"
      style="text-decoration:none; color:white;">Logout</a>
  </nav>
@@ -15,10 +15,11 @@
   <?php endif; ?> 
  <!-- sidebar -->
  <div class="sidenav">
-    <a href="../app/home.php">Home</a>
-    <a href="../app/services.php">Services</a>
+    <!-- <a href="../app/home.php">Home</a> -->
+    
     <a href="../app/orders.php">My Orders</a>
     <a href="../app/cards.php">My Cards</a>
+    <a href="../app/services.php">Services</a>
     <a href="../app/contact.php">Contact Us</a>
   </div>
 
